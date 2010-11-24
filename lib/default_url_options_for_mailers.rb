e66367fed03621bf74e7704aa8444e1e8e1b3136
Joe Moore <joe@pivotallabs.com>
2010-11-24 11:22:41 -0800

    More cleanup.  Example initializer for Rails projects.

--- lib/default_url_options_for_mailers.rb
+++ lib/default_url_options_for_mailers.rb
@@ -1,4 +1,2 @@
-#require "rails"
 require File.expand_path(File.join(File.dirname(__FILE__),'default_url_options_for_mailers','default_url_options_for_mailers'))
 require File.expand_path(File.join(File.dirname(__FILE__),'default_url_options_for_mailers','initializer'))
-#require 'default_url_options_for_mailers/default_url_options_for_mailers'
--- spec/rails_app/config/initializers/pivotal.rb
+++ spec/rails_app/config/initializers/pivotal.rb
@@ -1,3 +1,16 @@
 require File.expand_path(File.join(__FILE__, '../../../../../lib/default_url_options_for_mailers'))
 
+PivotalCore::Initializer::DefaultUrlOptionsForMailers.run do |config|
+  # *** Default URL Options For Mailers ***
+  # Use this line to automatically derive protocol/host/port using incoming rails request:
+  #  PivotalCore::Initializer::DefaultUrlOptionsForMailers.run
+  #  config.action_controller.default_url_options_for_mailers = :infer_url_options
 
+  # Use this line to explicitly set protocol/host/port:
+  # config.action_controller.default_url_options_for_mailers = { :protocol => "http", :host => "example.com", :port => 3333 }
+
+  # Comment out both lines to disable any default URL option functionality for mailers.
+
+  # NOTE: ActionMailer::Base.default_url_options always takes precedence over this facility and if *any* options are
+  # set there, default_url_options_for_mailers does nothing.
+end
